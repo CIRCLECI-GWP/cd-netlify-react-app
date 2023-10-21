db57ea466ac6afd3bfed3cd01cbdb28c2ea9c64c
yemiwebby <yemiwebby@gmail.com>
2023-10-21 18:20:04 +0100

    Added config details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Successfully Deployed <code>A React application</code> to Netlify with CircleCI
         </p>
         <a
           className="App-link"
